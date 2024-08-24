24d9c9747545c95a78d2d0e1d1e9101a5c7990fc
Gerrit Grunwald <han.solo@mac.com>
2024-08-24 08:06:42 +0200

    Initial Commit

---
+++ OnTravel/OnTravelApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnTravelApp.swift
+//  OnTravel
+//
+//  Created by Gerrit Grunwald on 24.08.24.
+//
+
+import SwiftUI
+
+@main
+struct OnTravelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
